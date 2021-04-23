fdd5130e3e3611960c1795eb96517f9d0aea06c9
Mohamed Sahnoun <mohamed.sahnoun.professional>
2021-04-23 17:49:11 +0100

    make it better3

---
+++ history.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'fragments/header.php';
+include_once 'autoload.php';
+$historyRepository = new HistoryRepository();
+$personnes = $historyRepository->findAll();
+
+?>
+
+<table class="table">
+    <tr>
+        <th>ByWho</th>
+        <th>Date</th>
+        <th>Type</th>
+        <th>Modification</th>
+
+    </tr>
+    <?php foreach ($personnes as $personne) {
+
+        ?>
+        <tr>
+            <td ><?= $personne->bywho?></td>
+            <td><?= $personne->date ?></td>
+            <td><?= $personne->type ?></td>
+            <td> <?= $personne->modification ?></td>
+
+
+
+
+
+
+
+        </tr>
+        <?php
+
+    }
+    ?>
+</table>
+</body>
+</html>
